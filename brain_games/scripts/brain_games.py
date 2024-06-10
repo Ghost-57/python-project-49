f9f02bbe462099e91883329a2173325683117287
Misha Shevtsov <masterbjj1996@gmail.com>
2024-06-10 20:21:45 +1000

    10.2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-from ..cli import welcome_user
-
+from brain_games.games.brain_games import welcome_user
 
 def main():
-    print("Welcome to the Brain Games!")
     welcome_user()
 
-
-if __name__ == "__main__":
+if name == '__main__':
     main()
